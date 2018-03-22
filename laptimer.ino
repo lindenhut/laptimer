62a481b82c182e582acc9a3fa8ad1b9e1379938f
Michiharu Nakazawa <lindenhut@gmail.com>
2018-03-23 01:13:47 +0900

    Update laptimer.ino

--- laptimer.ino
+++ laptimer.ino
@@ -8,7 +8,7 @@ int base_dist;
 int ave_dist = 0;
 int lap = 0;
 float start_time = 0;
-
+char s[16];
 void setup()
 {
   int error;
@@ -32,52 +32,62 @@ void setup()
   lcd.setBacklight(255);
   lcd.home(); lcd.clear();
   lcd.print("Now initializing....");
+  
   int i = 0;
   while(i<10){
   base_dist = analogRead(0);
   ave_dist += base_dist;
-  lcd.setCursor(0, 0);
+  lcd.setCursor(0, 2);
   lcd.print((6762/(base_dist-9))-4);
   lcd.print("cm");
   delay(100);
   i++;
   }
   ave_dist = ave_dist/10;
-  lcd.setCursor(0, 1);
+  lcd.setCursor(0, 3);
   lcd.print("Ave: ");
   lcd.print((6762/(ave_dist-9))-4);
   lcd.print("cm");
 
-  delay(3000);
+
   
-  lcd.clear();
+  lcd.home();
   if(ave_dist <400){
-    lcd.print("Too far. Press reset and try again.");
+    lcd.print("Failed to init. Please reset.");
   }else{
-  lcd.print("Ready to go!");
+    lcd.print("Ready to go!       ");
   }
+delay(3000);
+lcd.clear();
 }
 
+float last_time = 0.0;
 void loop()
 {
-int value = analogRead(0);
-  //valueがave_distから100変化したら反応
+float value = analogRead(0);
   lcd.setCursor(10, 1);
-  if(ave_dist - value >= 100){
-    start_time = millis()/1000;
-    lcd.print("Lap:");
+  start_time = millis()/1000.0;
+  lcd.print("L");
+  lcd.print(lap);
+  lcd.print("> ");
+  lcd.print(start_time);
+  if(ave_dist - value >= 50){
+    start_time = millis()/1000.0;
+    lcd.setCursor(10, 2);
+    lcd.print("L");
     lcd.print(lap);
-    lcd.print(" >> ");
-    lcd.print(start_time);
-    lcd.print(" sec");
-  lcd.setCursor(0, 4);
-    lcd.print(value);
-    lcd.print("V");
+    lcd.print("> ");
+    lcd.print(start_time - last_time);
+    lap ++;
+    last_time = start_time;
     delay(1000);
   }
   else{
   //  lcd.print("not found");
   }
-
+  lcd.setCursor(0, 4);
+//  lcd.print(dtostrf(value*5/1024, 5, 2, s));
+  lcd.print(value);
+  lcd.print("V");
 delay(50);
 }
